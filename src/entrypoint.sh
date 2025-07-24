d3b21772d51cf102699dee96ff783bf43269c0c0
DrummyFloyd <jonathan.monnet28@gmail.com>
2025-07-24 22:57:23 +0200

    chore(dockerfile): update according to refactor

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 check_vars() {
-  var_names=("$@")
-  for var_name in "${var_names[@]}"; do
-    [ -z "${!var_name}" ] && echo "$var_name is unset." && var_unset=true
+  local var_unset=false
+  for var_name in "$@"; do
+    if [ -z "${!var_name}" ]; then
+      echo "$var_name is unset."
+      var_unset=true
+    fi
   done
-  [ -n "$var_unset" ] && exit 1
+  $var_unset && exit 1
   return 0
 }
 
 check_vars CRUI_WATCH_NAMESPACE
 
-if [ "${CRUI_DEBUG}" = "true" ]; then
-  kopf run --liveness=http://0.0.0.0:8080/healthz --namespace "${CRUI_WATCH_NAMESPACE}" userinit.py --verbose
-else
-  kopf run --liveness=http://0.0.0.0:8080/healthz --namespace "${CRUI_WATCH_NAMESPACE}" userinit.py
-fi
+kopf_cmd="kopf run --liveness=http://0.0.0.0:8080/healthz --namespace ${CRUI_WATCH_NAMESPACE} userinit/userinit.py"
+[ "${CRUI_DEBUG}" = "true" ] && kopf_cmd+=" --verbose"
+
+exec $kopf_cmd
